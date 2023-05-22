720e800efb2ca155eab1b236cdc1a3f5f3345717
Filip Kahm <fika0302@storsjogymnasiet.se>
2023-05-22 14:33:37 +0200

    dela

---
+++ dela.py
@@ -0,0 +1,8 @@
+#välkommen
+print ("\nkolla om ätt tal är jämt eller udda\n") 
+var = input("\nmata in ett tal\n")
+if int(var) % 2 == 0:
+    print ("\ntalet är jämt\n")
+else: 
+    print("\nTalet är udda\n")
+
